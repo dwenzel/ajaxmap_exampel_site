48ce98045b4c5d820d019794f9a10a6973d93aed
Dirk Wenzel <wenzel@cps-it.de>
2019-06-04 14:36:42 +0200

    [TASK] prevent from calling obsolete middleware handlers

---
+++ Configuration/RequestMiddlewares.php
@@ -0,0 +1,61 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2019 Dirk Wenzel
+ *  All rights reserved
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the text file GPL.txt and important notices to the license
+ * from the author is found in LICENSE.txt distributed with these scripts.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+return [
+    'frontend' => [
+        'typo3/cms-adminpanel/data-persister' => [
+            'disabled' => true
+        ],
+        'typo3/cms-adminpanel/renderer' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/shortcut-and-mountpoint-redirect' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/page-argument-validator' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/maintenance-mode' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/static-route-resolver' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/base-redirect-resolver' => [
+            'disabled' => true
+        ],
+        'typo3/cms-adminpanel/initiator' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/site' => [
+            'disabled' => true
+        ],
+        'typo3/cms-adminpanel/sql-logging' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/backend-user-authentication' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/eid' => [
+            'disabled' => true
+        ],
+        'typo3/cms-frontend/timetracker' => [
+            'disabled' => true
+        ]
+    ]
+];
